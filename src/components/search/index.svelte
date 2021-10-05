716a7ac9a386cd3d5943c5222152178b5f15c4ce
Tanner <thetannerthomas@gmail.com>
2021-10-04 20:59:16 -0600

    moved comment

--- src/components/search/index.svelte
+++ src/components/search/index.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
 	let value: string = '';
 	let placeholder: string = 'New York City';
 	let isVisible: boolean = true;
@@ -12,13 +10,6 @@
 	function resetValue() {
 		value = '';
 	}
-
-	// onMount(() => {
-	// 	setInterval(() => {
-	// 		if (placeholder === 'New York City') return (placeholder = 'Salt Lake City');
-	// 		if (placeholder === 'Salt Lake City') return (placeholder = 'New York City');
-	// 	}, 2000);
-	// });
 
 	$: if (value.length >= 1) {
 		isVisible = false;
@@ -33,18 +24,20 @@
 </form>
 
 <style lang="postcss">
-	/* TODO: fix outline/ring/border input focus*/
 	form {
 		@apply flex bg-neutral rounded-md transition-all duration-200;
 		@apply hover:bg-black hover:bg-opacity-50;
 	}
+
 	input {
+		/* TODO: fix outline/ring/border input focus */
 		@apply bg-transparent rounded-md pl-2 transition-all duration-200 rounded-l-lg rounded-r-none;
 		@apply focus:border-none focus:ring-0 focus:ring-offset-0 focus:outline-none;
 	}
 	input::placeholder {
 		@apply opacity-40;
 	}
+
 	button {
 		@apply rounded-l-none;
 	}
